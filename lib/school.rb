be27e4eeb319438cc5a8c895dd9246f5aeb549f4
Joanna E K <kalemajoanna@gmail.com>
2019-12-18 18:20:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,32 @@
 # code here!
 
-class School
+# class School
  
-    def initialize(school)
-    @school = school
+#     def initialize(school)
+#     @school = school
+#     @roster = {}
+#   end
+
+  
+#   def add_student(name, grade)
+#   end 
+  
+# end 
+
+class School
+  attr_accessor :roster, :student_name, :school_name
+  def initialize(school_name)
+    @school_name = school_name
     @roster = {}
   end
 
-  
-  def add_student(name, grade)
-  end 
-  
-end 
+  def add_student(student_name, grade)
+    if @roster[grade] == true
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
 
+end
