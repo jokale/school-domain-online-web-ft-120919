829629ec8e8f5a8c0906de77dde9148178e95d83
Joanna E K <kalemajoanna@gmail.com>
2019-12-18 19:16:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,42 +1,42 @@
-# code here!
+# # code here!
+
+# # class School
+ 
+# #     def initialize(school)
+# #     @school = school
+# #     @roster = {}
+# #   end
+
+  
+# #   def add_student(name, grade)
+# #   end 
+  
+# # end 
 
 # class School
- 
-#     def initialize(school)
-#     @school = school
+#   attr_accessor :roster, :student_name, :school_name, :grade 
+  
+#   def initialize(student_name)
+#     @student_name = student_name
 #     @roster = {}
 #   end
 
   
-#   def add_student(name, grade)
-#   end 
-  
-# end 
-
-class School
-  attr_accessor :roster, :student_name, :school_name, :grade 
-  
-  def initialize(student_name)
-    @student_name = student_name
-    @roster = {}
-  end
-
-  
-  def add_student(student_name, grade)
-    if roster = [grade] == true 
-      roster = [grade] << student_name
-      else
-      @roster[grade] = []
-      @roster[grade] << student_name
-    end
-  end
+#   def add_student(student_name, grade)
+#     if roster = [grade] == true 
+#       roster = [grade] << student_name
+#       else
+#       @roster[grade] = []
+#       @roster[grade] << student_name
+#     end
+#   end
       
 
     
-    def grade
-    end 
+#     def grade
+#     end 
     
-    def sort 
-    end 
+#     def sort 
+#     end 
    
-end
+# end
